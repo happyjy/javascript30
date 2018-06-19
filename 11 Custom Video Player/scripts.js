5d48f78888b93bcf8b39b471b089c6142d6bdc8c
happyjy <okwoyjy@gmail.com>
2018-06-19 23:36:44 +0900

    study list

--- 11 Custom Video Player/scripts.js
+++ 11 Custom Video Player/scripts.js
@@ -1,12 +1,14 @@
-//index
+//<<>>index
 //1. Get Our Elements
 //2. Build out functions
 //3. Hook up the event Listeners
 
 
-//### html5 video tag method/properties   
+//<<>>ㅣ학 html5 video tag method/properties   
 //https://www.w3schools.com/tags/ref_av_dom.asp 
 
+
+//<<>>lecture step list 
 //stpe3 updateButton ► / ❚ ❚
 //fn : updateButton
 //event : video.play/pause
@@ -44,7 +46,7 @@ const ranges = player.querySelectorAll('.player__slider');
 /* Build out functions */
 //step2 : tooglePlay();
 // paused : video elements attribute
-/* ***learn1 turnery operator
+/* *** Study turnery operator ( [] )
     elements[variables].attibute
 */
 function togglePlay(){
@@ -55,7 +57,7 @@ function togglePlay(){
 
 function updateButton(){
     console.log("working? updateButton fn")
-    /* *** learn  
+    /* *** Study this in temrs of events 
         this : it's bound to the video elements it self (by queyrSelector)
     */
     const icon = this.paused ? '►' : '❚ ❚';
@@ -64,8 +66,11 @@ function updateButton(){
 }
 
 function skip(){
-    /** dataset
-     * first learn at lecture 03 CSS variables 
+    /* *** Study dataset
+     * def) on the HTMLElement interface provides read/write 
+     * access to all the custom data attributes (data-*) set on the element. 
+     * 
+     * 1. first Study at lecture 03 CSS variables 
      */
     console.log('working? skip' + JSON.stringify(this.dataset));
     video.currentTime += parseFloat(this.dataset.skip);
@@ -80,19 +85,21 @@ function handleRangeUpdate(){
 function handleProgress(){
     console.log('working? handleRangeUpdate : ');
     const percent = (video.currentTime / video.duration) * 100;
-    
-    //CSS propertiy
-    //flex : sets the flexible length on flexible items.
-    //flex-basis :  specifies the initial length of a flexible item.
+    /**
+     * *** Study CSS property(flex, flex-basis)
+     * flex : sets the flexible length on flexible items.
+     * flex-basis : specifies the initial length of a flexible item.
+     */
+
     progressBar.style.flexBasis = `${percent}%`;
 }
 
 function scrub(e){
-    /* learn offsetX
-    : offsetX value is progress elements positioned ralative, when you clicked
+    /* 
+     * *** Study offsetX
+    : def) offsetX value is progress elements positioned ralative, when you clicked
     : eg) value of first of progress bar : 1px
     : value of last of progress bar : progress.offsetWidth px
-    
     */
     console.log('### e.offsetX : ' + e.offsetX);
     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
@@ -116,7 +123,7 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove',  (e) => mousedown && scrub(e));
 /**
  * ### same as above line
- * *** learn explain : hijacking the fact that we can use '&&' 
+ * *** Study - && : hijacking the fact that we can use '&&' 
  * and if this is "true" it will move on to the scrub(e) 
  * (if this is "false" it will not move on the the scurb(e))
  * 
